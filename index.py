2fb7ad0141efb4b2862639e81c2b603d8985f4e6
yhoungdev <yhoungdev@gmail.com>
2024-03-12 12:18:30 +0100

    test code

---
+++ index.py
@@ -0,0 +1,20 @@
+import base64
+import mimetypes
+import os
+import subprocess
+import sys
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.template import Template
+
+
+subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True, cwd="/home/user")
+def bad3(request):
+    response = Template.render(request, 'vulnerable/xss/form.html', globals())
+    response.set_cookie(key='monster test', value='omnomnomnomnom!')
+    return response
+
+def file_access(request):
+    msg = request.GET.get('msg', '')
